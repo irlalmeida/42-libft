77461c4f2c5c00bd4fc98b704c9bc7700b6b28a2
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 17:44:03 -0300

    Changes to ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 16:35:25 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 17:36:41 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 17:37:20 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,8 @@ char *ft_strchr(const char *s, int c)
         if (s[counter] == c)
             return (char *)s+counter;
         counter++;
-    } 
+    }
     s = NULL; 
     return (char *)s;
 
-	
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 12:34:45 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/20 20:07:07 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 17:43:38 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ char *ft_strrchr(const char *s, int c)
     int counter;
 
     counter = 0;
-    while (s[counter] != '\0')
+    while (s[counter] != '\0' || s[counter] == c)
     {
         s++;
         counter++;
