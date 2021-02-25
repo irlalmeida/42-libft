6f0c58e168db4f5511bbb22fe4204270d65bd9b7
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 20:49:48 -0300

    Changes to ft_strncmp

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,24 +6,30 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 18:45:06 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 20:45:27 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 20:49:17 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
-    size_t counter;
+	size_t counter;
+    unsigned char *dst_type;
+    unsigned char *src_type;
 
     counter = 0;
-    while (counter < dstsize)
+    dst_type = (unsigned char *)dst;
+    src_type = (unsigned char *)src;
+
+	if (!dst_type && !src_type)
+		return(0);	
+
+	if (counter == dstsize)
+		return (dstsize);
+
+    while ((counter < dstsize))
     {
-        dst[counter] = src[counter];
-        counter++;
-    }
-    while (counter < dstsize)
-    {
-        dst[counter] = '\0';
+        dst_type[counter] = src_type[counter];
         counter++;
     }
     return (counter);
