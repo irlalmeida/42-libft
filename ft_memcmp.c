d72e184b5e6ccf4ed006917cf3b0d4050bb5aece
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 13:36:31 -0300

    Changes to ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:54:33 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 13:33:15 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 13:36:24 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,10 @@ int ft_memcmp(const void *b1, const void *b2, size_t len)
     b1_type = (unsigned char *)b1;
     b2_type = (unsigned char *)b2;
 	a = 0;
-    while (a < len && b1_type[a] == b2_type[a])
+
+	if (len == 0)
+		return (0);
+    while (a <= len && b1_type[a] == b2_type[a])
     {
        b1_type++;
        b2_type++;
