bff21ea07f92beeb2c4095995a666a27e6b79219
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 17:36:48 -0300

    Changes to ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 16:35:25 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 17:36:00 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 17:36:41 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,24 +16,12 @@ char *ft_strchr(const char *s, int c)
     int counter;
 
     counter = 0;
-	//while (s[counter])
-	//{
-	//	if (s[counter] == c)
-	//		return (char *)s + counter;
-	//	counter++;
-	//}
-	//if (s[counter] == '\0')
-	//	return (char *)s;
-	//return(NULL);
-	
 	while (s[counter] != '\0' || s[counter] == c)
     {
         if (s[counter] == c)
             return (char *)s+counter;
         counter++;
     } 
-	//if (s[counter] == c)
-	//	return (char *)s+counter;
     s = NULL; 
     return (char *)s;
 
