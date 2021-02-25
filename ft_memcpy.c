f4c96eb0ffd67b937cad24b828dc9bb7a1a114e6
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 14:54:32 -0300

    Changes to ft_memcmp

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:53:16 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 13:29:06 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:53:53 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@ void *ft_memcpy(void *dst, const void *src, size_t len)
     counter = 0;
     dst_type = (unsigned char *)dst;
     src_type = (unsigned char *)src;
+
+	if (!dst_type && !src_type)
+		return(0);	
 
 	if (counter == len)
 		return (dst);
