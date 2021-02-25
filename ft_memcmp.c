1fc4659dae60b1bd50f00ad24c7d048a0be2c97e
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 14:28:54 -0300

    Changes to ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,19 +6,19 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:54:33 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 14:26:28 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:28:48 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 int ft_memcmp(const void *b1, const void *b2, size_t len)
 {
-    unsigned int *b1_type;
-    unsigned int *b2_type;
+    unsigned char *b1_type;
+    unsigned char *b2_type;
     size_t counter;
     
-    b1_type = (unsigned int *)b1;
-    b2_type = (unsigned int *)b2;
+    b1_type = (unsigned char *)b1;
+    b2_type = (unsigned char *)b2;
 	counter = 0;
 
 	if (len == 0 || b1_type == b2_type || b1 == NULL || b2 == NULL)
@@ -27,7 +27,7 @@ int ft_memcmp(const void *b1, const void *b2, size_t len)
     {
 		if (b1_type != b2_type)
 			return (b1_type - b2_type);
-    
+	counter++;
 	}
 	return (0);
 }
