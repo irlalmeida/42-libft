5b891bbc89f09f75ff2c8b4c094b841aa3453c85
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 14:26:43 -0300

    Changes to ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,24 +6,24 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:54:33 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 13:57:37 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:26:28 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 int ft_memcmp(const void *b1, const void *b2, size_t len)
 {
-    unsigned char *b1_type;
-    unsigned char *b2_type;
-    size_t a;
+    unsigned int *b1_type;
+    unsigned int *b2_type;
+    size_t counter;
     
-    b1_type = (unsigned char *)b1;
-    b2_type = (unsigned char *)b2;
-	a = 0;
+    b1_type = (unsigned int *)b1;
+    b2_type = (unsigned int *)b2;
+	counter = 0;
 
 	if (len == 0 || b1_type == b2_type || b1 == NULL || b2 == NULL)
 		return (0);
-    while (a <= len)
+    while (counter <= len)
     {
 		if (b1_type != b2_type)
 			return (b1_type - b2_type);
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,23 +6,23 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 12:59:07 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/20 12:59:11 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 13:59:31 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 int ft_strncmp(const char *s1, const char *s2, size_t len)
 {
-    unsigned int a;
+    size_t counter;
 
-    a = 0;
-    while (a < len && (s1[a] != '\0' || s2[a] != '\0'))
+    counter = 0;
+    while (counter < len && (s1[counter] != '\0' || s2[counter] != '\0'))
     {
-        if (s1[a] != s2[a])
+        if (s1[counter] != s2[counter])
         {
-            return (s1[a] - s2[a]);
+            return (s1[counter] - s2[counter]);
         }
-        a++;
+        counter++;
     }
     return (0);
 }
