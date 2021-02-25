481fdd71f4b6e58faffceb10cf9db474016490ff
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 20:27:06 -0300

    Changes to ft_strrchr

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 12:34:45 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 20:22:10 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 20:25:58 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 char *ft_strrchr(const char *s, int c)
 {
-    size_t counter;
+    int counter;
 	size_t len;
 	len = ft_strlen(s);
-	counter = len-1;
-	while (counter > 0)
+	counter = len;
+	while (counter >= 0)
     {
         if (s[counter] == c)
             return (char *)s+counter;
