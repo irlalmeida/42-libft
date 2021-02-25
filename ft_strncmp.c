12b5b45e137aaf5497f6d118e474a846a5bcb96b
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 20:31:36 -0300

    Changes to ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/20 12:59:07 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 13:59:31 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 20:31:21 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 int ft_strncmp(const char *s1, const char *s2, size_t len)
 {
     size_t counter;
+	unsigned char *s1_type;
+	unsigned char *s2_type;
+	s1_type = (unsigned char *)s1;
+	s2_type = (unsigned char *)s2;
 
     counter = 0;
-    while (counter < len && (s1[counter] != '\0' || s2[counter] != '\0'))
+    while (counter < len && (s1_type[counter] != '\0' || s2_type[counter] != '\0'))
     {
-        if (s1[counter] != s2[counter])
+        if (s1_type[counter] != s2_type[counter])
         {
-            return (s1[counter] - s2[counter]);
+            return (s1_type[counter] - s2_type[counter]);
         }
         counter++;
     }
