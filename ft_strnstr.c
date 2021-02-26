9e99afcd98351fb52f6601a31654200315af973f
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-26 14:00:48 -0300

    Changes to ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:01:38 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/26 13:57:38 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/26 14:00:41 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,12 @@ char *ft_strnstr(const char *big, const char *little, size_t len)
 		return ((char *)big);
     while (big[counter] != '\0' && counter != len)
     {
-        while (big[counter] == little[counter])
+        while (big[counter] == little[counter] || little[counter] == '\0')
         {
-            while (big[counter] == little[counter] || little[counter] != '\0')
-            {
-                if (little[counter] == '\0')
-                {
-                    return (char *)little; 
-                }
+            while (big[counter] == little[counter] && little[counter] != '\0')
 				counter++;
-            }
     	}
+		return (char *)little; 
 	}
 	return(0);
 }
