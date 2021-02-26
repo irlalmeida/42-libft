86b90fee7acb5392348ddf48eaf94cdb978a8e6f
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-26 16:54:41 -0300

    Changes made to ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:56:17 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/26 16:52:16 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/26 16:54:38 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ int	ft_atoi(const char *nptr)
 	else
 		negnum = 1;
 	while (nptr[i] == '\v' ||nptr[i] == '\n'|| nptr[i] == '\t' || \
-	nptr[i] == '\f'||nptr[i] == '\r')
+	nptr[i] == '\f'||nptr[i] == '\r' ||nptr[i] == ' ')
 		i++;
 	if (nptr[i] >= '0' && nptr[i] <= '9')
 	{
