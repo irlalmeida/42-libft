d87422f2496f703cb0f4ac542a834959c66cc876
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-26 16:33:50 -0300

    Changes to ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,63 +6,37 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 13:01:38 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/26 16:30:28 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/26 16:33:46 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-//char *ft_strnstr(const char *big, const char *little, size_t len)
-//{
-//    //size_t i;
-	//size_t j;
-	//
-    //i = 0;
-	//j = 0;
-	//if ((len == 0) || (little[0] == '\0'))
-	//	return ((char *)big);
-	//if (little > big)
-	//	return (NULL);
-    //while (i != len)
-    //{
-    //    if (big[i] == little[j])
-    //    {
-    //        while (little[j] != '\0')
-    //        {
-    //            if (little[j+1] == '\0')
-    //            {
-    //                return ((char *)big + i - j); 
-    //            }
-	//			j++;
-	//			i++;
-    //        }
-    //	}
-	//	i++;
-	//}
-	//return (0);
-
-char    *ft_strnstr(const char *m, const char *n, size_t len)
+char *ft_strnstr(const char *big, const char *little, size_t len)
 {
-    unsigned char    *m1;
-    unsigned char    *n1;
-    size_t            i;
-    size_t            j;
-
-    m1 = (unsigned char *)m;
-    n1 = (unsigned char *)n;
+    size_t i;
+	size_t j;
+	
     i = 0;
-    if (!n1[i])
-        return ((char *)&m1[i]);
-    while (i < len && m1[i])
+	j = 0;
+	if ((len == 0) || (little[0] == '\0'))
+		return ((char *)big);
+	if (little > big)
+		return (NULL);
+    while (i != len)
     {
-        j = 0;
-        while ((i + j) < len && m1[i + j] == n1[j])
+        if (big[i] == little[j])
         {
-            if (!n1[j + 1])
-                return ((char *)&m1[i]);
-            j++;
-        }
-        i++;
-    }
-    return (0);
+            while (little[j] != '\0')
+            {
+                if (little[j+1] == '\0')
+                {
+                    return ((char *)big + i - j); 
+                }
+				j++;
+				i++;
+            }
+    	}
+		i++;
+	}
+	return (0);
 }
-
