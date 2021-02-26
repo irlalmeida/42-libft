8099e54803771495f4ce7bafa72db9253c66750f
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-26 16:56:17 -0300

    Changes made to ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/14 21:56:17 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/26 16:54:38 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/26 16:56:12 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,16 @@ int	ft_atoi(const char *nptr)
 	res = 0;
 	negnum = 0;
 	i = 0;
-	if (nptr[0] == '-')
+	while (nptr[i] == '\v' ||nptr[i] == '\n'|| nptr[i] == '\t' || \
+	nptr[i] == '\f'||nptr[i] == '\r' ||nptr[i] == ' ')
+		i++;
+	if (nptr[i] == '-')
 	{
 		negnum = -1;
 		i++;
 	}
 	else
 		negnum = 1;
-	while (nptr[i] == '\v' ||nptr[i] == '\n'|| nptr[i] == '\t' || \
-	nptr[i] == '\f'||nptr[i] == '\r' ||nptr[i] == ' ')
-		i++;
 	if (nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		while ((nptr[i] != '\0') && (nptr[i] >= '0' && nptr[i] <= '9'))
