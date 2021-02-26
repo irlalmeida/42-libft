8336f00d2482bb96e72065b987f106a6ec571382
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-25 22:47:07 -0300

    Changes to ft_strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 18:45:06 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/25 20:49:17 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/25 22:47:05 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,17 @@
 size_t ft_strlcpy(char *dst, const char *src, size_t dstsize)
 {
 	size_t counter;
-    unsigned char *dst_type;
-    unsigned char *src_type;
 
     counter = 0;
-    dst_type = (unsigned char *)dst;
-    src_type = (unsigned char *)src;
-
-	if (!dst_type && !src_type)
-		return(0);	
-
-	if (counter == dstsize)
-		return (dstsize);
-
-    while ((counter < dstsize))
+	if (!dst || !src)
+		return (0);
+	if (dstsize == 0)
+		return (ft_strlen(src));
+    while ((src[counter] && counter < dstsize-1))
     {
-        dst_type[counter] = src_type[counter];
+        dst[counter] = src[counter];
         counter++;
     }
-    return (counter);
+	dst[counter] = '\0';
+    return (src[counter]);
 }
