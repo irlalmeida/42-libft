cf1244f6d0de86b31592a5b7cb09b869afb4dec1
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-27 18:14:36 -0300

    Added fix to ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 13:41:02 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/27 18:09:31 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/27 18:14:32 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,23 +21,20 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	i = 0;
 	j = 0;
 	a = 0;
-	while (s1[i] != 0)
+	while (s1[i] != '\0')
 		i++;
-	while (s2[j] != 0)
+	while (s2[j] != '\0')
 		j++;
 	a = malloc(i + j);
-	while (*a < (i + j))
+	while (s2[j] != '\0')
 	{
-		while (s2[j] != '\0')
+		while (s1[i] != '\0')
 		{
-			while (s1[i] != '\0')
-			{
-				a[i] = s1[i];
-				i++; 
-			}
-		a[i + j] = s2[j];
-		j++;
+			a[i] = s1[i];
+			i++; 
 		}
+	a[i + j] = s2[j];
+	j++;
 	}
 	a[i + j] = '\0';
 	return (a);
