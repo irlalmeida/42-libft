7e7a7bc073d3044b0225673b4e123631965372b5
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-02-27 10:08:04 -0300

    Added fix to ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/27 09:31:33 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/02/27 10:07:05 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/02/27 10:08:03 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	counter = start;
 	size = 0;
-	while ((s[counter] != '\0') && (counter < len))
+	while ((s[counter] != '\0') && (counter <= len))
 		counter++;
 	sub = malloc(len + 1);
 	if (!sub)
