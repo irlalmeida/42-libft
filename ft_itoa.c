fbce4aa7343103f58137d8d1c110a98e35e7419a
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-01 23:40:20 -0300

    Added fix to ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,26 +6,44 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 12:35:59 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/01 22:39:48 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/01 23:39:41 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
-#include <stdio.h>
+
+char	*find_num(int n, int *len)
+{
+	char	*num;
+
+	num = 0;
+	if (n < 0)
+	{
+		(*len)++;
+		num = (char *)ft_calloc((*len + 1), sizeof(char));
+		if (!num)
+			return (NULL);
+		num[0] = '-';
+	}
+	else
+	{
+		num = (char *)ft_calloc((*len + 1), sizeof(char));
+		if (!num)
+			return (NULL);
+	}
+	return (num);
+}
 
 char	*ft_itoa(int n)
 {
-	char *num;
-	unsigned int nbr;
-	int len;
-	int nbr_n;
+	char		*num;
+	long int	nbr;
+	int			len;
+	int			nbr_n;
 
 	nbr = n;
 	len = 0;
 	nbr_n = nbr;
-	num = 0;
-
 	if (n == 0)
 		return ("0");
 	while (nbr_n != 0)
@@ -33,31 +51,14 @@ char	*ft_itoa(int n)
 		nbr_n /= 10;
 		len++;
 	}
+	num = find_num(n, &len);
 	if (n < 0)
-	{
-		nbr = (nbr * -1);
-		len++;
-		num = (char *)ft_calloc((len + 1), sizeof(char));
-			if (!num)
-				return (NULL);
-		num[0] = '-';
-	}
-	else
-		num = (char *)ft_calloc((len + 1), sizeof(char));
-	
-	if (num == NULL)
-		return (NULL);
-	while ((len > 0) && (nbr !=0))
+		nbr = (nbr * (-1));
+	while ((len > 0) && (nbr != 0))
 	{
 		num[len - 1] = ((nbr % 10) + '0');
 		len--;
 		nbr /= 10;
-		//printf("len %i\n",len);
 	}
 	return (num);
 }
-
-/*int main (void)
-{
-	printf("eai foi? %s\n", ft_itoa(-1234));
-}*/
