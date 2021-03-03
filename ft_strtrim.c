d994cb019ad045655584bcfe25074501fd641c9d
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-02 22:47:07 -0300

    Added fix to  ft_strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/01 14:11:41 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/02 22:38:21 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/02 22:46:01 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	while (len > 0 && (ft_strchr(set, s1[len])))
 		len--;
 	if ((len - i + 1) <= 0)
-		return (ft_strdup(0));
+		return (ft_strdup(""));
 	return (ft_substr(s1, i, (len - i + 1)));
 }
