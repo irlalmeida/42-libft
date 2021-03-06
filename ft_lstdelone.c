b6ae4d20f68b851fc13e79a776dc474893f0a5c0
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-06 18:18:01 -0300

    Changes made to ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 15:27:58 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/06 18:16:47 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/06 18:17:58 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	t_list *temp;
-
-	temp = lst->next;
 	free(lst);
 	del(lst);
-	lst->next = temp;
 }
