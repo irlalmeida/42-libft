ee3c5792badc8362f2cd2b7d8a19fb6a4b41052d
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-06 16:36:28 -0300

    Added fix to bonus function

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 15:13:22 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/06 16:32:24 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/06 16:36:25 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void ft_lstadd_back(t_list **lst, t_list *new)
 {
+	if (!lst)
+		return  ;
 	ft_lstlast(*lst)->next = new;
 }
