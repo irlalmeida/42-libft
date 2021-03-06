e735ed622f3b7b87837e8c3ed1d8ec56db16b7cd
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-06 18:05:45 -0300

    Changes made to fit the Norme

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 15:27:58 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/06 17:32:45 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/06 18:05:40 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void (ft_lstdelone(t_list *lst, void (*del)(void *)))
 {
-	
+	t_list *temp;
+
+	lst->next = temp;
+	free(lst);
+	del(lst);
+	temp->next = lst;
 }
