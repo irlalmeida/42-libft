4cc86e080229323388fc3a023f6ff175aa1cf762
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-06 16:30:26 -0300

    Added bonus functions

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 15:09:43 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/06 15:14:21 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/06 16:30:22 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list *ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (NULL);
 	while (lst)
 	{
 		lst = lst->next;
