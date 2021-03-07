969cc91b4f7cb1bd7b21250e4af7adcd1165ea8f
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-06 23:19:01 -0300

    Changes made to ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,17 +6,18 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 21:21:52 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/06 23:16:06 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/06 23:18:58 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list *head;
 	t_list *temp;
-	if ((!lst) || (!f))
+	
+	if (!lst)
 		return (NULL);
 	head = NULL;
 	while (lst)
@@ -28,7 +29,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 			return (NULL);
 		}
 		ft_lstadd_back(&head, temp);
-		lst->next = lst;
+		lst = lst->next;
 	}
 	return (head);
 }
