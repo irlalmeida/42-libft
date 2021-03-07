d32f8c2b6f34338b611302ba4c114bc11dd9f818
Isabela de Almeida <isabelarlalmeida@gmail.com>
2021-03-06 23:13:27 -0300

    Changes made to ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: iribeiro <iribeiro@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/06 21:21:52 by iribeiro          #+#    #+#             */
-/*   Updated: 2021/03/06 23:11:25 by iribeiro         ###   ########.fr       */
+/*   Updated: 2021/03/06 23:13:23 by iribeiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *),void (*del)(void *))
 		ft_lstclear(&head, del);
 		return (NULL);
 	}
-	while (lst != NULL)
+	while (lst)
 	{
 		ft_lstadd_back(&head, temp);
 		lst->next = lst;
